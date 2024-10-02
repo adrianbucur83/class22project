0d6f0083bd7d85d1f3b92daa86a3dd770e3239d0
Adrian Bucur <adrianbucur83@gmail.com>
2024-10-02 07:49:55 +0300

    class work 1 oct part 2

--- src/main/java/com/siit/class22project/config/SecurityConfig.java
+++ src/main/java/com/siit/class22project/config/SecurityConfig.java
@@ -17,6 +17,8 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/v3/api-docs, /v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**")
+                            .permitAll()
                         .requestMatchers("/api/**")
                             .permitAll()
                         .requestMatchers("/mvc/**")
@@ -31,6 +33,7 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         return new BCryptPasswordEncoder();
     }
 
--- src/main/java/com/siit/class22project/model/User.java
+++ src/main/java/com/siit/class22project/model/User.java
@@ -2,10 +2,12 @@ package com.siit.class22project.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "users")
 @Getter
+@Setter
 public class User {
 
     @Id
